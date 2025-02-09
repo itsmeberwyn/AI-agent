70c605768c274568ba021eb141460c92e996ad17
itsmeberwyn <bherfelismenia@gmail.com>
2025-02-09 12:45:25 +0800

    :sparkles: Initial Commit -- with integration of the following: -- integration of clerk for fast authentication -- convex for handling nosql database -- radix-ui for icons -- shadcn for presets

---
+++ convex/chats.ts
@@ -0,0 +1,74 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createChat = mutation({
+  args: {
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not Authenticated!");
+
+    const chat = await ctx.db.insert("chats", {
+      title: args.title,
+      userId: identity.subject,
+      createdAt: Date.now(),
+    });
+
+    return chat;
+  },
+});
+
+export const listChats = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not Authenticated!");
+
+    const chats = await ctx.db
+      .query("chats")
+      .withIndex("by_user", (q) => q.eq("userId", identity.subject))
+      .order("desc")
+      .collect();
+
+    return chats;
+  },
+});
+
+export const deleteChat = mutation({
+  args: {
+    id: v.id("chats"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not Authenticated!");
+
+    const messages = await ctx.db
+      .query("messages")
+      .withIndex("by_chat", (q) => q.eq("chatId", args.id))
+      .collect();
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const getChat = query({
+  args: {
+    id: v.id("chats"),
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not Authenticated!");
+    try {
+      const chat = await ctx.db.get(args.id);
+      if (!chat || chat.userId !== args.userId) return null;
+      return chat;
+    } catch (error) {
+      return null;
+    }
+  },
+});
